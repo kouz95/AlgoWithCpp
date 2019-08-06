4be59a1cd42663936d9e4063c29fd1b615af4705
kouz <lastpuzly@gmail.com>
2019-08-06 21:25:27 +0900

    need to study dfs & bfs

---
+++ BOJ/6603.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int t;
+int input[12] = {0,};
+void in()
+{
+    cin >> t;
+    for (int i = 0 ; i < t ; i++)
+        cin >> input[i];
+}
+void func()
+{
+    int res[12] = {0,};
+    
+}
