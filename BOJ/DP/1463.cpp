6e302bd35bb09c2c152f63bd25d66af870dfb6cc
kouz <lastpuzly@gmail.com>
2019-08-10 17:55:41 +0900

    done

--- BOJ/DP/1463.cpp
+++ BOJ/DP/1463.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    int n ;
-    
-    cin >> n ;
-    
-    int dp[1000001];
-    
+    int num;
+    int dp[1000001] = {0,};
+    cin >> num;
     
     dp[0] = 0;
     dp[1] = 0;
     dp[2] = 1;
     dp[3] = 1;
-    for (int i = 4 ; i <= n ; i ++)
-    {
+    
+    for (int i = 4 ; i <= num ; i++){
         int tmp = 0;
-        if (i%3 == 0)
-        {
-            if (!tmp || tmp > dp[i/3] + 1)
+        
+        if(i%3 == 0){
+            if(!tmp || tmp > dp[i/3] + 1)
                 tmp = dp[i/3] + 1;
         }
-        if (i%2 == 0)
-        {
-            if (!tmp || tmp > dp[i/2] + 1)
-                tmp = dp[i/2] + 1;
-        }
-        if (!tmp || tmp > dp[i-1] +1)
+        if(i%2 == 0 && (!tmp || tmp > dp[i/2] + 1 ))
+            tmp = dp[i/2] + 1;
+        if(!tmp || tmp > dp[i-1] + 1)
             tmp = dp[i-1] + 1;
         
-        dp[i]= tmp;
+        dp[i] = tmp;
     }
-    cout << dp[n];
+    cout << dp[num] <<endl;
 }
