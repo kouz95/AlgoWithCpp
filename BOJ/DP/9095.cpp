f26427fb3821a4d4ead9b4b61261d93ec20e573f
kouz <lastpuzly@gmail.com>
2019-08-10 23:59:41 +0900

    done

---
+++ BOJ/DP/9095.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int dp[11];
+int n;
+void init(){
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= n ; i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+    }
+}
+
+void func(){
+    cin >> n;
+    init();
+    cout << dp[n]<< endl;
+}
+
+void input(){
+    int t;
+    cin >> t;
+    while (t){
+        func();
+        t--;
+    }
+}
+int main(){
+    input();
+}
