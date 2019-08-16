cc85b1f83ccfe70532df4394944ba4193a289c0b
kouz <lastpuzly@gmail.com>
2019-08-16 18:25:06 +0900

    done

---
+++ BOJ/Math/1085.cpp
@@ -0,0 +1,9 @@
+//
+//  1085.cpp
+//  BOJ
+//
+//  Created by 김경준 on 16/08/2019.
+//  Copyright © 2019 kouz. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ BOJ/Math/2163.cpp
@@ -0,0 +1,24 @@
+//
+//  2163.cpp
+//  BOJ
+//
+//  Created by 김경준 on 16/08/2019.
+//  Copyright © 2019 kouz. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int n, m;
+
+void func(){
+    cout << n*m-1 << endl;
+}
+
+void init(){
+    cin >> n >> m;
+}
+
+int main(){
+    init();
+    func();
+}
