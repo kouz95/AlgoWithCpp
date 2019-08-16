bebcf87d71b4b7b089b89d40dfca4ae048d577bd
kouz <lastpuzly@gmail.com>
2019-08-16 18:25:21 +0900

    done

--- BOJ/Math/1085.cpp
+++ BOJ/Math/1085.cpp
@@ -6,4 +6,23 @@
 //  Copyright © 2019 kouz. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int x , y , w , h;
+
+void func(){
+    int minW= min(x,w-x);
+    int minH= min(y,h-y);
+    
+    cout << min(minW,minH) << endl;
+}
+
+void init(){
+    cin >> x >> y >> w >> h;
+}
+
+int main(){
+    init();
+    func();
+}
