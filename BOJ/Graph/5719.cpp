d39910a1da84d81ebd1da18e1f54ef5eb7044064
kouz <lastpuzly@gmail.com>
2019-08-28 18:02:11 +0900

    done

--- BOJ/Graph/5719.cpp
+++ BOJ/Graph/5719.cpp
@@ -2,51 +2,102 @@
 //  5719.cpp
 //  BOJ
 //
-//  Created by 김경준 on 22/08/2019.
+//  Created by 김경준 on 28/08/2019.
 //  Copyright © 2019 kouz. All rights reserved.
 //
 
 #include <iostream>
 #include <vector>
-#include <utility>
+#include <queue>
+#include <string.h>
 using namespace std;
+#define INF 2e9
+#define MAX 500
 
-#define INF 1000000
+int map[MAX][MAX];
 
-int node , edge;
-int map[500][500];
-int u, v, p;
-int dist[500];
-bool visit[500] = {false,};
-int sNode, eNode;
+vector<pair<int, int> > path[MAX];
+queue<int> Nodes;
 
-void getShortestPath(){
-    dist[0] = 0;
-    for (int i = 1 ; i < 500 ; i++)
-        dist[i] = INF;
-    int curNode = 0;
-    for(int i = 1 ; i < node ; i++){
-        dist[i] = dist[curNode] +
-    }
-    
+void initCost(vector<pair<int, int> > * cost, int vertex,int * dist){
+    for(int i = 0 ; i < vertex; i++)
+        cost[i].clear();
+    for(int i = 0 ; i < vertex ; i++)
+        for(int j = 0 ; j < vertex; j++)
+            if (map[i][j] != INF)
+                cost[i].push_back({j,map[i][j]});
+    fill(dist,dist+vertex,INF);
 }
-
-void init(){
-    cin >> node >> edge;
-    if(!node && !edge){
-        cin >> sNode >> eNode;
-        for (int i = 0 ; i < node ; i++)
-            for (int j = 0 ; j < node ; j++)
-                map[i][j] = i==j ? 0 : INF;
-        for (int i = 0 ; i < edge ; i++){
-            cin >> u >> v >> p;
-            map[u][v] = p;
+void deleteShortestPath(int startVertex,int endVertex,vector<pair<int, int> > * cost, int * dist){
+    Nodes.push(endVertex);
+    while(!Nodes.empty()){
+        int currNode = Nodes.front();
+        Nodes.pop();
+        for (int i = 0 ; i < path[currNode].size(); i++){
+            int from = path[currNode][i].first;
+            int to = path[currNode][i].second;
+            map[from][to] = INF;
+            Nodes.push(from);
         }
-        getShortestPath();
     }
 }
+
+void dijkstra(int vertex,int startVertex,vector<pair<int,int> > * cost,int * dist){
+    priority_queue<pair<int,int> > qu;
+    qu.push({0,startVertex});
+    dist[startVertex] = 0;
+    
+    while(!qu.empty()){
+        int currVertex = qu.top().second;
+        int currCost = -qu.top().first;
+        qu.pop();
+        for(int i = 0 ; i < cost[currVertex].size(); i++){
+            int nextVertex = cost[currVertex][i].first;
+            int nextCost = cost[currVertex][i].second;
+            if(dist[nextVertex] > dist[currVertex] + nextCost){
+                dist[nextVertex] = dist[currVertex] + nextCost;
+                qu.push({-dist[nextVertex],nextVertex});
+                path[nextVertex].clear();
+                path[nextVertex].push_back({currVertex,nextVertex});
+            }
+            if (dist[nextVertex] == dist[currVertex] + nextCost)
+                path[nextVertex].push_back({currVertex,nextVertex});
+        }
+    }
+}
+
 
 int main(){
-    init();
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int vertex, edge, startVertex, endVertex;
+    vector<pair<int,int> > cost[MAX];
+    int dist[MAX];
+    while(1){
+        cin >> vertex >> edge >> startVertex >> endVertex;
+        if(!vertex && !edge)
+            break;
+        for(int i = 0 ; i < MAX; i++)
+            cost[i].clear();
+        fill(&map[0][0],&map[MAX-1][MAX],INF);
+        for(int i = 0 ; i < edge ; i++){
+            int from , to , val;
+            cin >> from >> to >> val ;
+            cost[from].push_back({to,val});
+            map[from][to] = val;
+        }
+        fill(dist,dist+vertex,INF);
+        dijkstra(vertex,startVertex,cost,dist);
+        deleteShortestPath(startVertex,endVertex,cost,dist);
+        initCost(cost,vertex,dist);
+        dijkstra(vertex,startVertex,cost,dist);
+        for(int i = 0 ; i < MAX; i++)
+            path[i].clear();
+        if(dist[endVertex] == INF)
+            cout << -1 << "\n";
+        else cout << dist[endVertex] << "\n";
+        
+    }
 }
 
