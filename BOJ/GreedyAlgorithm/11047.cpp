f77957116c0601ef56cd303cd42623fb309929b6
kouz <lastpuzly@gmail.com>
2019-08-29 13:18:15 +0900

    done

--- BOJ/GreedyAlgorithm/11047.cpp
+++ BOJ/GreedyAlgorithm/11047.cpp
@@ -6,4 +6,29 @@
 //  Copyright © 2019 kouz. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int func(int n, int k, int * worth){
+    int res = 0;
+    for(int i = n-1 ; i >= 0 ; i--)
+        while(k >= worth[i]){
+            k -= worth[i];
+            res++;
+        }
+    return res;
+}
+
+int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, k;
+    cin >> n >> k;
+    int worth[n];
+    for(int i = 0 ; i < n ; i++)
+        cin >> worth[i];
+    cout << func(n,k,worth) << "\n";
+}
