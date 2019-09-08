b73353bcab843eaecc56bed9a229ae6d129e9b06
kouz <lastpuzly@gmail.com>
2019-09-08 15:59:55 +0900

    fail

--- BOJ/Graph/1707.cpp
+++ BOJ/Graph/1707.cpp
@@ -6,4 +6,31 @@
 //  Copyright © 2019 kouz. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void func(vector<int> * adjacent){
+    vector<int> Group1;
+    vector<int> Group2;
+    
+    
+}
+
+int main(){
+    int k;
+    cin >> k;
+    while(k--){
+        int vertex, edge;
+        cin >> vertex, edge;
+        vector<int> adjacent[vertex+1];
+        for(int i = 0 ; i < edge ; i++){
+            int from, to;
+            cin >> from >> to;
+            adjacent[from].push_back(to);
+        }
+        func(adjacent);
+    }
+}
+
