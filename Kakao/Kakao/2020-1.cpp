c4b55c3fb30f484c79da44199874ffa64a5a785f
kouz <lastpuzly@gmail.com>
2019-10-28 18:21:37 +0900

    done

--- Kakao/Kakao/2020-1.cpp
+++ Kakao/Kakao/2020-1.cpp
@@ -1,9 +1,41 @@
-//
-//  2020-1.cpp
-//  Kakao
-//
-//  Created by 김경준 on 28/10/2019.
-//  Copyright © 2019 kouz. All rights reserved.
-//
+#include <string>
+#include <vector>
 
-#include <stdio.h>
+using namespace std;
+
+int slice(string s, int step){
+    int cnt = 1;
+    string resStr = "";
+    for(int i = 0 ; i < s.length(); i+=step){
+        if(i + 2 * step > s.length()){
+            if(cnt > 1)
+                resStr += to_string(cnt) + s.substr(i);
+            else resStr += s.substr(i);
+            break;
+        }
+        string currStr = s.substr(i,step);
+        string nextStr = s.substr(i+step,step);
+        if(currStr == nextStr){
+            cnt++;
+            continue;
+        }
+        if(cnt > 1){
+            resStr += to_string(cnt) + currStr;
+            cnt = 1;
+            continue;
+        }
+        resStr += currStr;
+    }
+    return resStr.length();
+}
+
+int solution(string s){
+    int answer = s.length();
+    
+    for(int i = 1 ; i <= s.length()/2; i++){
+        if(answer > slice(s, i)){
+            answer = slice(s, i);
+        }
+    }
+    return answer;
+}
