60cedd998fb20467f26f77e9dc351b40da18a260
kouz <lastpuzly@gmail.com>
2019-10-29 00:25:04 +0900

    no Message

---
+++ Kakao/Kakao/tmp.cpp
@@ -0,0 +1,17 @@
+//
+//  tmp.cpp
+//  Kakao
+//
+//  Created by 김경준 on 2019/10/28.
+//  Copyright © 2019 kouz. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a, b = 0;
+    cout << a + b << endl;
+}
