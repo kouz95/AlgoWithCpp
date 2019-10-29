05aba50ad2c1d175ae2e327de678006822415042
kouz <lastpuzly@gmail.com>
2019-10-29 15:24:27 +0900

    done

--- Kakao/Kakao/2020-2.cpp
+++ Kakao/Kakao/2020-2.cpp
@@ -7,79 +7,56 @@ string cutAndReverse(string s){
     for(int i = 1 ; i < s.length()-1 ; i++){
         if(s[i] == '(')
             s[i] = ')';
-        if(s[i] == ')')
+        else if(s[i] == ')')
             s[i] = '(';
     }
     return s.substr(1,s.length()-2);
 }
 
 bool isNiceStr(string s){
-    bool isNice = false;
-    string tmp;
+    bool res = false;
+    int cnt = 0 ;
     for(int i = 0 ; i < s.length(); i++){
-        if(s.length() == 0){
-            isNice = true;
+        if(s[i] == '(')
+            cnt++;
+        else if(s[i] == ')')
+            cnt--;
+        if(cnt < 0)
             break;
-        }
-        if(i == 0 && s[i] == ')')
-            return isNice;
-        if(i == s.length()-1)
-            break;
-        if(s[i] == '(' && s[i+1] == ')'){
-            s = s.substr(0,i) + s.substr(i+2);
-            i-=1;
-            continue;
-        }
     }
-    return isNice;
+    if(cnt == 0)
+        res = true;
+    return res;
 }
 
-int separateStr(string s){
-    int sepIdx = 0;
-    int leftCnt = 0;
-    int rightCnt = 0;
+int firstBalanceIdx(string s){
+    int res = 0;
+    int cnt = 0;
     for(int i = 0 ; i < s.length(); i++){
-        if(s[i] == '('){
-            leftCnt++;
-        }
-        if(s[i] == ')'){
-            rightCnt++;
-        }
-        if(leftCnt != 0 && rightCnt != 0 && leftCnt == rightCnt){
-            sepIdx = i;
+        if(s[i] == '(')
+            cnt++;
+        else if(s[i] == ')')
+            cnt--;
+        if(cnt == 0){
+            res = i;
             break;
         }
     }
-    return sepIdx;
+    
+    return res;
 }
+
 
 string solution(string p) {
     string answer = "";
-    int loopCnt = 0;
-    bool goLoop = false;
     if(p.length() == 0)
-        return answer;
-    int sepIdx = separateStr(p);
-    string u = p.substr(0,sepIdx+1);
-    string v;
-    if(sepIdx + 1 >= p.length())
-        v = "";
-    else
-        string v = p.substr(sepIdx+1);
-    if(goLoop)
-        return u;
-    if(isNiceStr(u) && loopCnt == 0){
-        goLoop = true;
-        u += solution(v);
-        goLoop = false;
-        loopCnt++;
+        return "";
+    int separateIdx = firstBalanceIdx(p) + 1;
+    string u = p.substr(0,separateIdx);
+    string v = p.substr(separateIdx);
+    if(isNiceStr(u)){
+        return u + solution(v);
     }
-    else{
-        answer = '(' + solution(v) + ')' + cutAndReverse(u);
-    }
-        
+    answer = '(' + solution(v) + ')' + cutAndReverse(u);
     return answer;
 }
-//(()())()
-
-// u (()()), v()
