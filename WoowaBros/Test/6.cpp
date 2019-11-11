01d34c9e8a665d31d2ff88d92b26bc1dd14edd60
kouz <lastpuzly@gmail.com>
2019-11-11 17:17:43 +0900

    fail : need to find the way to handing utf-8 (한글 입력시 c++ 문자열 처리 방법?)

--- WoowaBros/Test/6.cpp
+++ WoowaBros/Test/6.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 bool isDuplicatedWith(string compareStr, string currStr) {
     bool res = false;
-    for(int i = 0 ; i < (compareStr.length()/sizeof(compareStr[0])) - 1 ; i++) {
+    for(int i = 0 ; i < (compareStr.length()/sizeof("제") - 1) ; i++) {
         string firstStr = compareStr.substr(i, 2);
-        for(int j = 0 ; j < (currStr.length()/sizeof(currStr[0])) - 1 ; j++) {
+        for(int j = 0 ; j < (currStr.length()/sizeof("제") - 1) ; j++) {
             string secondStr = currStr.substr(j, 2);
             if(firstStr == secondStr)
                 return true;
@@ -38,4 +38,35 @@ vector<string> solution(vector<vector<string>> forms) {
         }
     }
     return answer;
+}
+
+int main() {
+    vector<string> tmpInputs;
+    vector<vector<string>> forms;
+    tmpInputs.push_back("jm@email.com");
+    tmpInputs.push_back("제이엠");
+    forms.push_back(tmpInputs);
+    tmpInputs.clear();
+    tmpInputs.push_back("jason@email.com");
+    tmpInputs.push_back("제이슨");
+    forms.push_back(tmpInputs);
+    tmpInputs.clear();
+    tmpInputs.push_back("woniee@email.com");
+    tmpInputs.push_back("워니");
+    forms.push_back(tmpInputs);
+    tmpInputs.clear();
+    tmpInputs.push_back("mj@email.com");
+    tmpInputs.push_back("엠제이");
+    forms.push_back(tmpInputs);
+    tmpInputs.clear();
+    tmpInputs.push_back("nowm@email.com");
+    tmpInputs.push_back("이제엠");
+    forms.push_back(tmpInputs);
+    tmpInputs.clear();
+    
+    vector<string> sol = solution(forms);
+    for(int i = 0 ; i < sol.size(); i++)
+        cout << sol[i] <<endl;
+    cout << sizeof("라") << endl;
+    
 }
