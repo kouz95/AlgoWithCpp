209752baf944dabe4520e7c71c57181221b98906
kouz <lastpuzly@gmail.com>
2019-11-11 17:15:57 +0900

    fix: 상품의 개수가 더 많을 때

--- WoowaBros/Test/3.cpp
+++ WoowaBros/Test/3.cpp
@@ -12,7 +12,7 @@ int solution(vector<int> prices, vector<int> discounts) {
     int answer = 0;
     sort(prices.begin(), prices.end(), greater<int>());
     sort(discounts.begin(), discounts.end(), greater<int>());
-    for(int i = 0 ; i < discounts.size(); i++) {
+    for(int i = 0 ; i < min(discounts.size(), prices.size()); i++) {
         prices[i] = getDiscountedPrices(discounts[i], prices[i]);
     }
     for(int i = 0 ; i < prices.size(); i++) {
